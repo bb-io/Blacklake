31a12c5f634f0a7ffdbc9fa0bfd7c921df6bb0b8
mathijs-bb <mathijs.sonnemans@blackbird.io>
2025-09-12 11:28:09 +0200

    Updated required inputs and improved error handling

---
+++ Apps.Blacklake/Models/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Blacklake.Models;
+public class Error
+{
+    public string Title { get; set; }
+    public int Status { get; set; }
+    public Dictionary<string, List<string>> Errors { get; set; }
+}
--- Tests.Blacklake/ActionTests.cs
+++ Tests.Blacklake/ActionTests.cs
@@ -8,13 +8,15 @@ namespace Tests.Blacklake;
 [TestClass]
 public class ActionTests : TestBase
 {
+    public const string LAKE_ID = "bfabed9b-b19a-4e12-9c6a-5d8c4b8493a8";
+
     [TestMethod]
     public async Task Commit()
     {
         var actions = new Actions(InvocationContext, new FileManager());
 
         var file = new FileReference { Name = "contentful.translated.xlf" };
-        await actions.Commit(new LakeInput { LakeId = "c3b93820-f79a-42fd-a654-bddff5378eaf" }, new CommitInput { File = file });
+        await actions.Commit(new LakeInput { LakeId = LAKE_ID }, new CommitInput { File = file });
     }
 
     [TestMethod]
@@ -22,8 +24,8 @@ public class ActionTests : TestBase
     {
         var actions = new Actions(InvocationContext, new FileManager());
 
-        var file = new FileReference { Name = "contentful.xlf" };
-        var result = await actions.Leverage(new LakeInput { LakeId = "c3b93820-f79a-42fd-a654-bddff5378eaf" }, new LeverageInput { File = file });
+        var file = new FileReference { Name = "contentful.html" };
+        var result = await actions.Leverage(new LakeInput { LakeId = LAKE_ID }, new LeverageInput { File = file, TargetVariant = "nl" });
 
         Console.WriteLine(result.File.Name);
         Console.WriteLine(result.File.ContentType);
