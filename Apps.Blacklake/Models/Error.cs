42f68d6d2f4863314a02eeae889578e64b021a77
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2026-09-23 13:15:17 +0200

    Added automatic retries for temporary server errors and clearer error messages

--- Apps.Blacklake/Models/Error.cs
+++ Apps.Blacklake/Models/Error.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Apps.Blacklake.Models;
 
-namespace Apps.Blacklake.Models;
 public class Error
 {
     public string Title { get; set; }
+    public string Detail { get; set; }
+    
     public int Status { get; set; }
+    
     public Dictionary<string, List<string>> Errors { get; set; }
 }
